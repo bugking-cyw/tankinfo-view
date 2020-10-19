dd2934da169b727c8e623be886ecc1944a593786
陈亚文 <chenyawen@chenyawendeMacBook-Pro.local>
2020-10-19 17:51:57 +0800

    删除不要的注释

--- src/api/user.js
+++ src/api/user.js
@@ -2,13 +2,7 @@ import request from '@/utils/request'
 // import { data } from 'autoprefixer'
 
 export function login(data) {
-  // return request({
-  //   url: '/vue-admin-template/user/login',
-  //   method: 'post',
-  //   data
-  // })
   return request({
-    // url: 'http://localhost/tankinfo-admin/admin/login',
     url: '/tankinfo-admin/admin/login',
     method: 'post',
     data: data
